e818a3dac2db81d618a8cec1b866a4bfb9cadefc
Patrick <prjadwin@pstcc.edu>
2018-11-20 16:09:37 -0500

    Added method to loop through text

--- src/sad_trello/SAD_Trello.java
+++ src/sad_trello/SAD_Trello.java
@@ -5,17 +5,33 @@
  */
 package sad_trello;
 
-/**
- *
- * @author patrick
- */
+import java.util.Scanner;
+
 public class SAD_Trello {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
+    public void HowMany(Object string) {
+        int amt = 0;
+        
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("Please enter how many times you would like to loop: ");
+        try {
+            amt = scn.nextInt();
+        } catch (Exception e) {
+            System.out.println("Not a digit");
+            System.exit(0);
+        } 
+        
+        System.out.print("\n");
+        
+        for (int i = 0; i < amt; i++) {
+            System.out.println(string.toString());
+        }
+    }
 }
