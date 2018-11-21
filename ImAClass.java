3d07abdbe522f106e79ccb5df6e715f6427da1fc
Matt Thomas <AtomicSushi@users.noreply.github.com>
2018-11-21 14:03:16 -0500

    Added ImAClass.java
    the ToString method prints "I am a class"

---
+++ ImAClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sad_trello;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mthomas3
+ */
+public class ImAClass {
+    String message;
+    
+    public ImAClass(){
+        message = "I'm a class";
+    }
+    
+    public void ToString(){
+        JOptionPane.showMessageDialog(null, message);
+    }
+    
+}
